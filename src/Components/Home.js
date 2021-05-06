3e2bf41d18104e1278c4ec43c9555e704441c3c2
sandeepmehta2155 <66156650+sandeepmehta2155@users.noreply.github.com>
2021-05-06 16:34:10 +0530

    Home.js added

---
+++ src/Components/Home.js
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+import { useVid } from "../videoProvider";
+import * as useComponent from "../index";
+
+export function Home() {
+  const { videoSrc, videoDispatch } = useVid();
+
+  return (
+    <>
+      <useComponent.Navigation />
+      <div className="videoBadges">
+        <ul className="videoList">
+          {videoSrc.map((obj) => (
+            <li
+              key={obj.videoDetails.id}
+              className="videoCard"
+              id={obj.videoDetails.id}
+              onClick={() => videoDispatch({ type: "addToHistory", obj })}
+            >
+              <Link to={`/video/${obj.videoDetails.id}`}>
+                <img
+                  className="videoSnippet"
+                  title={obj.videoDetails.title}
+                  src={obj.srcLinks?.imgSrc}
+                  alt="loading"
+                />
+                <h3>
+                  <img
+                    className="streamerIcon"
+                    src={obj.videoDetails.streamerIcon}
+                    alt="loading"
+                  />
+                  <span className="streamer"> {obj.videoDetails.streamer}</span>
+                </h3>
+                <div>{obj.videoDetails.title}</div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
